20ee0b907177c8629886ed9f0de1486d24404472
Dawid Janta Lipiński <djantalipinski@gmail.com>
2023-05-19 13:52:55 +0200

    feat: keeping the number of glasses in local storage

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,25 @@ console.log("hello world");
 const counter = document.querySelector('.counter__js');
 const buttonAdd = document.querySelector('.add__js');
 const buttonDelete = document.querySelector('.delete__js');
-
+const key = new Date().toLocaleString().slice(0, 10);
 let currentGlassCounter = 0;
+
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue){
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
+
+counter.innerHTML = currentGlassCounter;
+
 
 buttonAdd.addEventListener('click', () => {
     currentGlassCounter++;
     counter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
 });
 
 buttonDelete.addEventListener('click', () => {
@@ -16,4 +29,6 @@ buttonDelete.addEventListener('click', () => {
         currentGlassCounter = 0;
     }
     counter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
 });
+
